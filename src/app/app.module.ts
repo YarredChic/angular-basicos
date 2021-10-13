3ad4a66bd8ee49e3e56310d5189461de6fb2a53f
Yared Chic <yaredchicr@gmial.com>
2021-10-13 15:42:29 -0600

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { contadorModule } from './contador/contador.module';
+
+
+// tarea Crear un modulo llamado ContadorModule
+
+
+import { heroesModule } from './heroes/heroes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    heroesModule,
+    contadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core'
+import { contadorComponet } from './contador/contador.component';
+
+
+
+
+@NgModule ({
+    declarations: [
+       contadorComponet
+    ],
+    exports: [
+        contadorComponet,
+    ]
+})
+export class contadorModule {
+
+
+}
